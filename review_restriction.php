0ad48fba133a09f0222ba13f09851ddf4dd50c48
mbpf1090 <sebastian.weidele@gmail.com>
2020-12-21 07:48:34 +0000

    Replace form with text for reviewed products

--- review_restriction.php
+++ review_restriction.php
@@ -1,8 +1,8 @@
 <?php
-/*
- * Plugin Name:       Restrict reviews
+/* 
+ * Pluginn Name:       Restrict reviews
  * Description:       Customers can leave a review only once for each product.
- * Version:           1.0
+ * Version:           1.1
  * Author:            MBPF
 */
  
@@ -10,7 +10,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 
-function restrict_review($submit_button) {
+function has_reviewed() {
     //get user
     $user = wp_get_current_user();
     $user_id = $user->ID;
@@ -19,20 +19,30 @@ function restrict_review($submit_button) {
     $product_id = $product->id;
     //get user comments
     $comments = get_comments( array( 'user_id' => array($user_id), 'post_type' => 'product', 'post_id' => $product_id, ));
-    
-    //Debug
-    //print("<pre>".print_r($comments, true)."</pre>");
-    //print("<pre>".print_r($user_id, true)."</pre>");
-    //print("<pre>".print_r($user_id, true)."</pre>");
-    //print("<pre>".print_r($product_id, true)."</pre>");
-    //echo 'test' . var_dump($comments);
-    
-    //if already commented empty array
+    //check length of comments array
     if (count($comments) > 0) {
+        return true;
+    }
+    return false;
+    
+}
+
+function remove_submit_button($submit_button) {
+    if (has_reviewed() == true) {
         unset($submit_button);
+    }
+    return $submit_button;
+}
+
+function replace_comments_form($fields) {
+    if (has_reviewed() == true) {
+        unset($fields);
+        $fields = array('comment' => '<p>Sie haben dieses Produkt schon bewertet</p>');
+        return $fields;
     } else {
-        return $submit_button;
+        return $fields;
     }
 }
 
-add_filter('comment_form_submit_button', 'restrict_review' );
+add_filter('comment_form_submit_button', 'remove_submit_button' );
+add_filter('comment_form_fields', 'replace_comments_form' );
