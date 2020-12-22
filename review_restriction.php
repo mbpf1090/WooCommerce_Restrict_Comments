b9b8c5185b6ecd0f02e5f83eba0344f83123fb6b
mbpf1090 <sebastian.weidele@gmail.com>
2020-12-22 11:35:30 +0000

    Add GitHub url to plugin info

--- review_restriction.php
+++ review_restriction.php
@@ -1,17 +1,17 @@
 <?php
-/* 
- * Pluginn Name:	Restrict reviews
+/*
+ * Plugin Name:		Restrict reviews
  * Description:		Customers can leave a review only once for each product.
  * Version:		1.1
  * Author:		MBPF
-		 * PluginURI:		https://github.com/mbpf1090/WooCommerce_Restrict_Comments
+ * Plugin URI:		https://github.com/mbpf1090/WooCommerce_Restrict_Comments
 */
  
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 
-function has_reviewed() {
+function remove_form($form_fields) {
     //get user
     $user = wp_get_current_user();
     $user_id = $user->ID;
@@ -20,30 +20,36 @@ function has_reviewed() {
     $product_id = $product->id;
     //get user comments
     $comments = get_comments( array( 'user_id' => array($user_id), 'post_type' => 'product', 'post_id' => $product_id, ));
-    //check length of comments array
+    //if already commented empty array
     if (count($comments) > 0) {
-        return true;
+	return $form_fields['comment'] = array();
+    } else {
+        return $form_fields;
     }
-    return false;
-    
 }
 
 function remove_submit_button($submit_button) {
-    if (has_reviewed() == true) {
-        unset($submit_button);
-    }
-    return $submit_button;
-}
-
-function replace_comments_form($fields) {
-    if (has_reviewed() == true) {
-        unset($fields);
-        $fields = array('comment' => '<p>Sie haben dieses Produkt schon bewertet</p>');
-        return $fields;
+    //get user
+    $user = wp_get_current_user();
+    $user_id = $user->ID;
+    //get product
+    $product = get_product();
+    $product_id = $product->id;
+    //get user comments
+    $comments = get_comments( array( 'user_id' => array($user_id), 'post_type' => 'product', 'post_id' => $product_id, ));
+    //if already commented empty array
+    if (count($comments) > 0) {
+	unset($submit_button);
     } else {
-        return $fields;
+        return $submit_button;
     }
 }
-
+//add_filter( 'comment_form_defaults', 'wpse33039_form_defaults' );
+function wpse33039_form_defaults( $defaults )
+{
+	var_dump($defaults);
+    $defaults['title_reply'] = 'foo';
+    return $defaults;
+}
+add_filter('comment_form_fields', 'remove_form' );
 add_filter('comment_form_submit_button', 'remove_submit_button' );
-add_filter('comment_form_fields', 'replace_comments_form' );
